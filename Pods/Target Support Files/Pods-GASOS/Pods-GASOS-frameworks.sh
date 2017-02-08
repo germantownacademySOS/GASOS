b4e8e72026ab4c0b718c7539c98625723c54cba6
Perry Fraser <perry@frasers.org>
2017-02-07 20:20:43 -0500

    Revert "Made a button look awesome and added some icons."
    This reverts commit 2eefa81896e86c31913bba2c8d052a0322647ee5.

--- Pods/Target Support Files/Pods-GASOS/Pods-GASOS-frameworks.sh
+++ Pods/Target Support Files/Pods-GASOS/Pods-GASOS-frameworks.sh
@@ -91,13 +91,11 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Cephalopod/Cephalopod.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyButton/SwiftyButton.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Cephalopod/Cephalopod.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/SwiftyButton/SwiftyButton.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyJSON/SwiftyJSON.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
