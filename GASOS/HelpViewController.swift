6b9a88d72cef081b62e760b073b2655c84b2499d
Perry Fraser <perry@frasers.org>
2017-02-24 15:31:27 -0500

    Added Help and Info icon (icons8.com) added thanks to text

---
+++ GASOS/HelpViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HelpViewController.swift
+//  GASOS
+//
+//  Created by Perry Fraser on 2/24/17.
+//  Copyright © 2017 Perry Fraser. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+}
